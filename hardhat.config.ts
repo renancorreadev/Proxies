d1fa58fce8d8e1ae88654fe89ef0ea0e46584616
renan.franca.avanade <renan.franca.avanade@elo.com.br>
2023-11-16 10:45:27 -0300

    add key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,13 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
 
 
-const PRIVATE_KEY="6139eb4c7a004f2c90a3233b493517b52718f7ad5fd4a82ef326351dfaced7b5"
-const FFPrivateKey = ""
-const ETHERSCAN_API_KEY="ZASD27N3FJ8NJ7HD471SN93TKAYBEKSYRF" 
-const INFURA_API_KEY="fce92eb6f57546ef83a440a2f532829e"
+const PRIVATE_KEY = "6139eb4c7a004f2c90a3233b493517b52718f7ad5fd4a82ef326351dfaced7b5"
+const FFPrivateKey = "6d69dceeab29191cc966fc42b4cdc7fd7a301c7c77b5af3c4d3e6efb8436c31d"
+const ETHERSCAN_API_KEY = "ZASD27N3FJ8NJ7HD471SN93TKAYBEKSYRF"
+const INFURA_API_KEY = "fce92eb6f57546ef83a440a2f532829e"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20", 
+  solidity: "0.8.20",
   networks: {
     firefly: {
       url: "http://127.0.0.1:5100",
