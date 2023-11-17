99bd27482a9fa21e772abfe7ef8073d9174ae1f3
renan.franca.avanade <renan.franca.avanade@elo.com.br>
2023-11-17 16:04:08 -0300

    fix: update blockmonitor for blocks

--- blockmonitor/blockchain/monitor.go
+++ blockmonitor/blockchain/monitor.go
@@ -1,48 +1,64 @@
 package blockchain
 
-/** MonitorClientRegistrationEvents
- * A chamada client.SubscribeFilterLogs é utilizada para se inscrever nos logs de eventos do contrato.
-  Os eventos são recebidos através do canal logs e processados.
-	Para cada evento, parseEvent é chamada para extrair os dados do evento, e SaveEventToFile é usada para salvá-los no
-  arquivo especificado.
-*/
-
 import (
 	"context"
 	"log"
+	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func MonitorClientRegistrationEvents(client *ethclient.Client, contractAddress common.Address, contractAbi abi.ABI, filePath string) {
+func PollClientRegistrationEvents(client *ethclient.Client, contractAddress common.Address, contractAbi abi.ABI, filePath string) {
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{contractAddress},
 	}
 
-	logs := make(chan types.Log)
-	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
-	if err != nil {
-		log.Fatalf("Falha na inscrição de eventos: %s", err)
-	}
+	lastBlock := uint64(0)
+	blockRangeLimit := uint64(200) // Defina o número de blocos a serem verificados por vez
 
 	for {
-		select {
-		case err := <-sub.Err():
-			log.Fatal(err)
-		case vLog := <-logs:
-			event, err := parseEvent(contractAbi, vLog)
-			if err != nil {
-				log.Printf("Erro ao analisar o evento: %s", err)
-				continue
-			}
-			err = SaveEventToFile(event, filePath)
-			if err != nil {
-				log.Printf("Erro ao salvar o evento no arquivo: %s", err)
-			}
+		// Obtém o número do bloco mais recente
+		blockNumber, err := client.BlockNumber(context.Background())
+		if err != nil {
+			log.Printf("Erro ao obter o número do bloco mais recente: %s", err)
+			time.Sleep(10 * time.Second)
+			continue
 		}
+
+		if blockNumber > lastBlock {
+			for fromBlock := lastBlock; fromBlock < blockNumber; fromBlock += blockRangeLimit {
+				toBlock := fromBlock + blockRangeLimit
+				if toBlock > blockNumber {
+					toBlock = blockNumber
+				}
+
+				query.FromBlock = big.NewInt(0).SetUint64(fromBlock)
+				query.ToBlock = big.NewInt(0).SetUint64(toBlock)
+
+				logs, err := client.FilterLogs(context.Background(), query)
+				if err != nil {
+					log.Printf("Erro ao filtrar logs: %s", err)
+					break
+				}
+
+				for _, vLog := range logs {
+					event, err := parseEvent(contractAbi, vLog)
+					if err != nil {
+						log.Printf("Erro ao analisar o evento: %s", err)
+						continue
+					}
+					if err := SaveEventToFile(event, filePath); err != nil {
+						log.Printf("Erro ao salvar o evento no arquivo: %s", err)
+					}
+				}
+			}
+			lastBlock = blockNumber
+		}
+
+		time.Sleep(10 * time.Second)
 	}
 }
--- blockmonitor/main.go
+++ blockmonitor/main.go
@@ -20,7 +20,7 @@ type ContractABI struct {
 func main() {
 	log.Println("Starting block monitor...")
 
-	const ethereumNodeURL = "http://192.168.15.200:5100"
+	const ethereumNodeURL = "http://localhost:5100"
 	const contractAddressHex = "0xEB9e9E2DbC00fC320AC66413F169adD6abe7c222"
 	const filePath = "./fileOutput/test.json"
 	const abiPath = "../blockchain/artifacts/contracts/ClientManager.sol/ClientManager.json"
@@ -41,14 +41,41 @@ func main() {
 		log.Fatalf("Erro ao analisar a ABI: %v", err)
 	}
 
+	contractAddress := common.HexToAddress(contractAddressHex)
+
 	client, err := blockchain.ConnectToEthereumNode(ethereumNodeURL)
 	if err != nil {
 		log.Fatalf("Erro ao conectar ao nó Ethereum: %v", err)
 	}
 
-	contractAddress := common.HexToAddress(contractAddressHex)
+	// // Criação de um canal para escutar sinais de interrupção
+	// stopChan := make(chan os.Signal, 1)
+	// signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
 
-	go blockchain.MonitorClientRegistrationEvents(client, contractAddress, parsedABI, filePath)
+	// // Goroutine para monitoramento de blocos
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case <-stopChan: // Encerra a goroutine se um sinal de interrupção for recebido
+	// 			log.Println("Encerrando o monitoramento de blocos.")
+	// 			return
+	// 		case <-time.After(3 * time.Second):
+	// 			blockNumber, err := client.BlockNumber(context.Background())
+	// 			if err != nil {
+	// 				log.Printf("Erro ao obter o número do bloco mais recente: %v", err)
+	// 				// Implementar lógica de reintento aqui, se necessário
+	// 			} else {
+	// 				log.Printf("Número do bloco mais recente: %v", blockNumber)
+	// 			}
+	// 		}
+	// 	}
+	// }()
+
+	// // Aguarda por um sinal de interrupção
+	// <-stopChan
+	// log.Println("Programa encerrado.")
+
+	go blockchain.PollClientRegistrationEvents(client, contractAddress, parsedABI, filePath)
 
 	// Manter o programa em execução
 	<-context.Background().Done()
