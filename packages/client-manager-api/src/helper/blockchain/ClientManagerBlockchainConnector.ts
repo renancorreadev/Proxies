bfbff24dd5738ee318be2cf2eadef0b1d915e794
renan.franca.avanade <renan.franca.avanade@elo.com.br>
2023-11-24 14:39:53 -0300

    feat: update module name

--- packages/client-manager-api/src/helper/blockchain/ClientManagerBlockchainConnector.ts
+++ packages/client-manager-api/src/helper/blockchain/ClientManagerBlockchainConnector.ts
@@ -1,5 +1,5 @@
 import { JsonRpcProvider, Wallet } from 'ethers';
-import { CustomerManagementCore, ClientManager__factory } from '@smart-contracts';
+import { CustomerManagementCore, CustomerManagementCore__factory } from '@smart-contracts';
 
 export class ClientManagerBlockchainConnector {
 	protected contract: CustomerManagementCore;
@@ -9,6 +9,6 @@ export class ClientManagerBlockchainConnector {
 	constructor(contractAddress: string, provider: string, privateKey: string) {
 		this.provider = new JsonRpcProvider(provider);
 		this.wallet = new Wallet(privateKey, this.provider);
-		this.contract = ClientManager__factory.connect(contractAddress, this.wallet);
+		this.contract = CustomerManagementCore__factory.connect(contractAddress, this.wallet);
 	}
 }
