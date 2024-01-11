2deb68e132956d9af41920883abdf1836ab0f2f9
renan.franca.avanade <renan.franca.avanade@elo.com.br>
2024-01-11 17:06:35 -0300

    feat: update layout customer app

--- packages/customer-rewards-app/src/app/layout.tsx
+++ packages/customer-rewards-app/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './styles/globals.css'
-import { Sidebar } from '@/components/Sidebar'
+import { Header } from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="grid min-h-screen grid-cols-app ">
-          <Sidebar />
-          <main className="px-4 pb-12 pt-8">{children}</main>
+        <div className="">
+          <Header />
+          <main>{children}</main>
         </div>
       </body>
     </html>
