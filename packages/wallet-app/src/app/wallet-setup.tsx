6099d048014581014392f510fedda29f65716059
renancorrea.accenture <renancorrea.accenture@natura.net>
2024-10-20 23:59:14 -0300

    dded svg support as well as svg transformer, stack screen routes for create wallet

--- packages/wallet-app/src/app/wallet-setup.tsx
+++ packages/wallet-app/src/app/wallet-setup.tsx
@@ -4,38 +4,44 @@ import { Image } from "expo-image";
 import styled from "styled-components/native";
 import { useSession } from "../providers/wallet-provider";
 import Button from "../components/Button/Button";
+import { createWallet } from "../utils/createWallet";
+import { ThemeType } from "../styles/theme";
+import { setStorageItemAsync } from "../hooks/use-storage-state";
 
-const SafeAreaContainer = styled(SafeAreaView)`
+const SafeAreaContainer = styled(SafeAreaView)<{ theme: ThemeType }>`
   flex: 1;
   background-color: ${(props) => props.theme.colors.primary};
   justify-content: flex-end;
 `;
 
-const ContentContainer = styled.View`
+const ContentContainer = styled.View<{ theme: ThemeType }>`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
 
-const TextContainer = styled.View`
+const TextContainer = styled.View<{ theme: ThemeType }>`
   padding: ${(props) => props.theme.spacing.large};
 `;
 
-const Title = styled.Text`
+const Title = styled.Text<{ theme: ThemeType }>`
   font-family: ${(props) => props.theme.fonts.families.openBold};
   font-size: 32px;
   color: ${(props) => props.theme.fonts.colors.primary};
   margin-bottom: ${(props) => props.theme.spacing.small};
 `;
 
-const Subtitle = styled.Text`
+const Subtitle = styled.Text<{ theme: ThemeType }>`
   font-family: ${(props) => props.theme.fonts.families.openRegular};
   font-size: ${(props) => props.theme.fonts.sizes.large};
   color: ${(props) => props.theme.fonts.colors.primary};
 `;
 
-const ButtonContainer = styled.View`
-  padding: ${(props) => props.theme.spacing.large};
+const ButtonContainer = styled.View<{ theme: ThemeType }>`
+  padding-left: ${(props) => props.theme.spacing.large};
+  padding-right: ${(props) => props.theme.spacing.large};
+  padding-bottom: ${(props) => props.theme.spacing.large};
+  padding-top: ${(props) => props.theme.spacing.small};
 `;
 
 const ExpoImage = styled(Image)`
@@ -43,7 +49,7 @@ const ExpoImage = styled(Image)`
   width: 100%;
 `;
 
-const ImageContainer = styled.View`
+const ImageContainer = styled.View<{ theme: ThemeType }>`
   flex: 1;
   width: 100%;
   justify-content: center;
@@ -60,9 +66,9 @@ const SecondaryButtonContainer = styled.TouchableOpacity`
   border-radius: ${(props) => props.theme.borderRadius.large};
 `;
 
-const SecondaryButtonText = styled.Text`
+const SecondaryButtonText = styled.Text<{ theme: ThemeType }>`
   font-family: ${(props) => props.theme.fonts.families.openBold};
-  font-size: ${(props) => props.theme.fonts.sizes.large};
+  font-size: ${(props) => props.theme.fonts.sizes.header};
   color: ${(props) => props.theme.fonts.colors.primary};
 `;
 
@@ -70,9 +76,26 @@ export default function SignIn() {
   const { signIn } = useSession();
 
   const walletSetup = () => {
-    signIn();
-    router.replace("/");
+    const wallet = createWallet();
+
+    if (Object.keys(wallet).length > 0) {
+      const address = wallet.address;
+      const mnemonicPhrase = wallet.mnemonic.phrase;
+      const publicKey = wallet.publicKey;
+      const privateKey = wallet.privateKey;
+
+      // TODO: This is temporary and should be done in a
+      // more secure way using the secure store
+
+      // setStorageItemAsync("address", address);
+      // setStorageItemAsync("mnemonicPhrase", mnemonicPhrase);
+      // setStorageItemAsync("publicKey", publicKey);
+      // setStorageItemAsync("privateKey", privateKey);
+
+      router.push("/seed-phrase");
+    }
   };
+
   return (
     <SafeAreaContainer>
       <ContentContainer>
@@ -94,7 +117,9 @@ export default function SignIn() {
       <ButtonContainer>
         <Button onPress={walletSetup} title="Create Wallet" />
         <SecondaryButtonContainer onPress={() => console.log("importing...")}>
-          <SecondaryButtonText>Import Wallet</SecondaryButtonText>
+          <SecondaryButtonText>
+            Got a wallet? Let's import it
+          </SecondaryButtonText>
         </SecondaryButtonContainer>
       </ButtonContainer>
     </SafeAreaContainer>
--- packages/wallet-app/src/styles/theme.ts
+++ packages/wallet-app/src/styles/theme.ts
@@ -1,7 +1,57 @@
-const Theme = {
+export type ThemeType = {
+  colors: {
+    primary: string;
+    dark: string;
+    lightDark: string;
+    accent: string;
+    background: string;
+    highlight: string;
+  };
+  fonts: {
+    families: {
+      openRegular: string;
+      openBold: string;
+      robotoRegular: string;
+      robotoBold: string;
+    };
+    sizes: {
+      small: string;
+      normal: string;
+      large: string;
+      header: string;
+      title: string;
+    };
+    weights: {
+      normal: string;
+      bold: string;
+    };
+    colors: {
+      primary: string;
+      dark: string;
+      accent: string;
+      background: string;
+      highlight: string;
+    };
+  };
+  spacing: {
+    tiny: string;
+    small: string;
+    medium: string;
+    large: string;
+    huge: string;
+  };
+  borderRadius: {
+    small: string;
+    default: string;
+    large: string;
+  };
+};
+
+const Theme: ThemeType = {
   colors: {
     primary: "#8878F4",
     dark: "#191919",
+    lightDark: "#222222",
     accent: "#F97068",
     background: "#EDF2EF",
     highlight: "#D1D646",
