76b19ba868d8c45b299b1b4ec0f978b21f8c250d
renancorrea.accenture <skynancemusic@gmail.com>
2024-10-21 16:27:05 -0300

    feat: add login status global

--- packages/customer-rewards-app/src/pages/Home/OnLockPage/index.tsx
+++ packages/customer-rewards-app/src/pages/Home/OnLockPage/index.tsx
@@ -1,8 +1,12 @@
+import { useUserStore } from '@/store/store';
+
 export function HomeOnLockPage() {
+  const isLogged = useUserStore((state) => state.isLogged);
+
   return (
     <div className="flex center justify-center  my-5">
       <h2 className="text-3xl font-bold tracking-tight text-slate-600">
-        HomeOnLockPage
+        IsLogged: {isLogged.toString()}
       </h2>
     </div>
   );
--- packages/customer-rewards-app/src/store/store.ts
+++ packages/customer-rewards-app/src/store/store.ts
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 import { jwtDecode } from 'jwt-decode';
-
 interface TokenPayload {
   exp: number;
 }
@@ -8,17 +7,28 @@ interface TokenPayload {
 interface UserState {
   token: string | null;
   email: string | null;
+  isLogged: boolean;
   setUser: (email: string, token: string) => void;
+  logout: () => void;
   isTokenValid: () => boolean;
+  checkLoginStatus: () => void;
 }
 
 export const useUserStore = create<UserState>((set) => ({
   token: null,
   email: null,
+  isLogged: false,
+
   setUser: (email, token) => {
     sessionStorage.setItem('token', token);
     sessionStorage.setItem('email', email);
-    set({ email, token });
+    set({ email, token, isLogged: true });
+  },
+
+  logout: () => {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('email');
+    set({ token: null, email: null, isLogged: false });
   },
 
   isTokenValid: () => {
@@ -27,16 +37,28 @@ export const useUserStore = create<UserState>((set) => ({
 
     try {
       const decoded: TokenPayload = jwtDecode(token);
-
       const currentTime = Date.now() / 1000;
+
       if (decoded.exp > currentTime) {
-        set({ token, email: sessionStorage.getItem('email') });
+        set({
+          token,
+          email: sessionStorage.getItem('email'),
+          isLogged: true,
+        });
         return true;
       }
+
+      set({ isLogged: false });
       return false;
     } catch (error) {
       console.error('Error decoding token:', error);
+      set({ isLogged: false });
       return false;
     }
+  },
+
+  checkLoginStatus: () => {
+    const isValid = useUserStore.getState().isTokenValid();
+    set({ isLogged: isValid });
   },
 }));
