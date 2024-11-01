a7f3cd09872f69db1b3726a802f51e54cba0523c
renancorrea.accenture <skynancemusic@gmail.com>
2024-11-01 16:47:36 -0300

    Initialize subgraph

--- packages/loyahub-admin-ui/src/components/Sidebar.tsx
+++ packages/loyahub-admin-ui/src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { Braces, Home, SquareStack, Trophy } from 'lucide-react';
 import React, { useEffect, useRef, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 
-import Logo from '../images/logo/logo.svg';
+import Logo from '../images/logo/logo.png';
 import SidebarLinkGroup from './SidebarLinkGroup';
 
 interface SidebarProps {
@@ -66,9 +66,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       }`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
-      <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
+      <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5 ">
         <NavLink to="/">
-          <img src={Logo} alt="Logo" />
+          <img className="bg-gray-2 rounded-3xl" src={Logo} alt="Logo" />
         </NavLink>
 
         <button
--- packages/loyahub-doc/src/screens/Header/index.tsx
+++ packages/loyahub-doc/src/screens/Header/index.tsx
@@ -26,8 +26,8 @@ export function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/about/intro">
+            Introduction ⏱️
           </Link>
           <button
             className="button button--outline button--lg"
